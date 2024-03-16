27b8e6d46782a9c70c8f5ab6a4d7ec3a60ac457b
Enock Jeremi <enockjeremi@gmail.com>
2024-03-16 18:32:07 -0300

    fix to deploy 2

--- src/main.ts
+++ src/main.ts
@@ -34,6 +34,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(1342);
+  await app.listen(8080);
 }
 bootstrap();
